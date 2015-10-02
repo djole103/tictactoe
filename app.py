a346a0c83f4215e4b66e0eb113601bfbd69f5972
djole <djordje_7@hotmail.com>
2015-10-01 20:27:21 -0400

    menu, basic structure and flow

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+
+instructions = "Input position you would like to play in the format \"R C\" where R is the row and C is the column."
+
 class Match:
 	def __init__(self):
 		self.board = [[ ' ' , ' ' , ' '],
@@ -5,16 +9,38 @@ class Match:
              		  [ ' ' , ' ' , ' ']]
 
 		self.turn = "Player X"
+		self.status = "Ongoing"
 
 	def __repr__(self):
 		return "repr"
 
 	def __str__(self):
-		return "TIC TAC TOE!\nMake your move: %s\n%s\n%s\n%s" % (self.turn,repr(self.board[0]),repr(self.board[1]),repr(self.board[2]))
+		return "\nMake your move: %s\n%s\n%s\n%s" % (self.turn,repr(self.board[0]),repr(self.board[1]),repr(self.board[2]))
 
-def main():
+	def makeMove():
+		pass
+
+def startGame():
 	match = Match()
 	print(match)
+    while(match.status != finished):
+		match.makeMove()
+	
 
+def menu():
+	print("xxx TIC TAC TOE! ooo")
+	print("MENU\n")
+	print("1: Start Game!\n
+		   2: Instructions\n
+		   3: Quit")
+	option = int(input)
+	choices = { 1 : startgame(),
+				2 : print(instructions),
+				3 : sys.exit()
+
+def main():
+	menu()
+	
 if __name__ == "__main__":
-	main()    
+	main()
+    
